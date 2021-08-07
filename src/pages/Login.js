9687478353efa670a2c63c25ca0fb75e6d0cb8a2
James Clark <clarkey95@hotmail.com>
2021-08-07 12:45:27 -0400

    working on hp

---
+++ src/pages/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Login() {
+  return (
+    <div className="mainBody monstersBG">
+        <h1>Monster Battle</h1>
+        <form className="hello">
+        <input className="formInput">Username</input>
+        <input className="formInput">Password</input>
+        </form>
+    </div>
+  );
+}
